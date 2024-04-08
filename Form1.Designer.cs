0095a0184d5115e8d41a5f727511a487216015dc
Sevdaq <sevdaulukurt2@gmail.com>
2024-04-08 20:06:40 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Mayintarlasii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUret = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblmayin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUret
+            // 
+            this.btnUret.Location = new System.Drawing.Point(664, 0);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(124, 129);
+            this.btnUret.TabIndex = 0;
+            this.btnUret.Text = "Üret";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(658, 251);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(665, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SKOR";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Location = new System.Drawing.Point(731, 149);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(0, 16);
+            this.lblSkor.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(665, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MAYIN";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblmayin
+            // 
+            this.lblmayin.AutoSize = true;
+            this.lblmayin.Location = new System.Drawing.Point(734, 185);
+            this.lblmayin.Name = "lblmayin";
+            this.lblmayin.Size = new System.Drawing.Size(14, 16);
+            this.lblmayin.TabIndex = 5;
+            this.lblmayin.Text = "0";
+            this.lblmayin.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 278);
+            this.Controls.Add(this.lblmayin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnUret);
+            this.Name = "Form1";
+            this.Text = "1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUret;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblmayin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mayintarlasii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblSkor.Text = "0";
+        }
+
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            int mayin1 = 0;
+            int mayin2 = 0;
+            int mayin3 = 0;
+
+            Random rnd = new Random();
+            mayin1 = rnd.Next(1, 20);
+            mayin2 = rnd.Next(21, 40);
+            mayin3 = rnd.Next(41, 50);
+
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Button btnTemp = new Button();
+                btnTemp.Name = "btn" + i.ToString();
+                btnTemp.Size = new System.Drawing.Size(35, 35);
+                btnTemp.Text = i.ToString();
+                btnTemp.UseVisualStyleBackColor = true;
+                if (mayin1 == i || mayin2 == i || mayin3 == i)
+                {
+                    btnTemp.Tag = true;
+                }
+                else
+                {
+                    btnTemp.Tag = false;
+                }
+                btnTemp.Click += BtnTemp_Click;
+                flowLayoutPanel1.Controls.Add(btnTemp);
+            }
+        }
+
+        private void BtnTemp_Click(object sender, EventArgs e)
+        {
+            // eğer 3 kez mayına basıldı ise form üzerinde bulunan tüm butonları pasif edin ve uygulama bitti şeklinde bir mesaj versin. 
+            Button basilanButon = ((Button)sender);
+            bool mayinBulundumu = (bool)basilanButon.Tag;
+            if (mayinBulundumu)
+            {
+                MessageBox.Show("Mayını buldunuz tebrikler");
+                basilanButon.BackColor = Color.Red;
+                int mayinInt = int.Parse(lblmayin.Text);
+                mayinInt++;
+                lblmayin.Text = mayinInt.ToString();
+
+            }
+            else
+            {
+                basilanButon.BackColor = Color.Green;
+                int skorInt = int.Parse(lblSkor.Text);
+                skorInt++;
+                lblSkor.Text = skorInt.ToString();
+
+
+            }
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
